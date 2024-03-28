76aea3db3d5eb5c6cae1696f55cf7c08d651af75
Erwan <cuir-saunas.07@icloud.com>
2024-03-28 10:41:05 +0100

    **feature: Add code file for the blank template**
    _Commit description:_
    
    Add code file for the blank template
    
    [CR #001]

---
+++ src/AdventofCode.cpp
@@ -0,0 +1,6 @@
+#include "AdventOfCode.hpp"
+
+void AdventOfCode::displayAnswer([[maybe_unused]] const unsigned short& year, [[maybe_unused]] const unsigned short& day, [[maybe_unused]] const bool& isPartOne)
+{
+    throw UnavailableDateExeption();
+}
